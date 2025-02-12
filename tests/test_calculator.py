71fe62da475a7a3a85dc945de1e3de5681ac2df1
Kallam Jai Bharath Reddy <bharathreddy@Kallams-MacBook-Air.local>
2025-02-12 01:26:26 -0500

    initial commits of the files

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,9 @@
-'''My Calculator Test'''
-from calculator import Calculator
-
+'''my calculator'''
+from calculator import add, subtract
 def test_addition():
     '''Test that addition function works '''    
-    assert Calculator.add(2,2) == 4
+    assert add(5,3) == 8
 
 def test_subtraction():
     '''Test that addition function works '''    
-    assert Calculator.subtract(2,2) == 0
-
-def test_divide():
-    '''Test that division function works '''    
-    assert Calculator.divide(2,2) == 1
-
-def test_multiply():
-    '''Test that multiply function works '''    
-    assert Calculator.multiply(2,2) == 4
+    assert subtract(7,3) == 4
